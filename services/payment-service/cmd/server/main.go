69af1c2058d953125928a62d5d60f1d75e13bad7
safrizal <safrizalhk12@gmail.com>
2025-12-29 16:32:04 +0700

    Fix Midtrans Response (token)

--- services/payment-service/cmd/server/main.go
+++ services/payment-service/cmd/server/main.go
@@ -42,7 +42,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:8081
+// @host      localhost:8002
 // @BasePath  /api/v1
 
 // @securityDefinitions.basic BasicAuth
