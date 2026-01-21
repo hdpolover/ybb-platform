950d74b58687f0e78eab106c0d06209d5c43f9ef
hdpolover <hendrapolover@gmail.com>
2026-01-21 20:10:10 +0700

    feat: add metadata module with countries, states, cities, timezones, currencies, shirt sizes, genders, application categories, and dietary restrictions

---
+++ services/api/src/modules/metadata/metadata.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { ApplicationCategory, Gender, PaymentStatus } from '@prisma/client';
+import { Country, State, City } from 'country-state-city';
+import { 
+  CURRENCIES, 
+  DIETARY_RESTRICTIONS, 
+  SHIRT_SIZES, 
+  TIMEZONES 
+} from './metadata.constants';
+
+@Injectable()
+export class MetadataService {
+  getCountries() {
+    return Country.getAllCountries().map(country => ({
+      name: country.name,
+      isoCode: country.isoCode,
+      phonecode: country.phonecode,
+      currency: country.currency,
+      flag: country.flag,
+      latitude: country.latitude,
+      longitude: country.longitude,
+    }));
+  }
+
+  getStates(countryCode: string) {
+    return State.getStatesOfCountry(countryCode);
+  }
+
+  getCities(countryCode: string, stateCode?: string) {
+    if (!stateCode) {
+      return City.getCitiesOfCountry(countryCode);
+    }
+    return City.getCitiesOfState(countryCode, stateCode);
+  }
+
+  getTimezones() {
+    return TIMEZONES;
+  }
+
+  getCurrencies() {
+    return CURRENCIES;
+  }
+
+  getGenders() {
+    return Object.values(Gender);
+  }
+
+  getApplicationCategories() {
+    return Object.values(ApplicationCategory);
+  }
+
+  getShirtSizes() {
+    return SHIRT_SIZES;
+  }
+
+  getDietaryRestrictions() {
+    return DIETARY_RESTRICTIONS;
+  }
+
+  getPaymentStatuses() {
+    return Object.values(PaymentStatus);
+  }
+}
