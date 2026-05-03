c853357fd0b0be75e45d6ef389df65c50b8dfcc1
hdpolover <hendrapolover@gmail.com>
2026-05-04 01:19:10 +0700

    feat: add AuthModule to PartnershipsModule imports

--- services/api/src/modules/partnerships/partnerships.module.ts
+++ services/api/src/modules/partnerships/partnerships.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { PrismaModule } from '@shared/infrastructure/prisma/prisma.module';
+import { AuthModule } from '@modules/auth/auth.module';
 import { PartnershipsPublicController } from './presentation/controllers/partnerships.public.controller';
 import { PartnershipsAdminController } from './presentation/controllers/partnerships.admin.controller';
 import { GetPublicPartnershipsHandler } from './application/handlers/get-public-partnerships.handler';
 import { SubmitPartnershipEnquiryHandler } from './application/handlers/submit-partnership-enquiry.handler';
 
 @Module({
-  imports: [CqrsModule, PrismaModule],
+  imports: [CqrsModule, PrismaModule, AuthModule],
   controllers: [PartnershipsPublicController, PartnershipsAdminController],
   providers: [
     GetPublicPartnershipsHandler,
